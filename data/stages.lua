6ec78cb748a9eb426c3cfea98348998d8f9a33ad
Renan Chaves <renanchavess@gmail.com>
2025-10-23 01:06:41 -0300

    fix: stages

--- data/stages.lua
+++ data/stages.lua
@@ -2,13 +2,11 @@ experienceStages = {
         { minlevel = 1, maxlevel = 200, multiplier = 50, },
         { minlevel = 201, maxlevel = 400, multiplier = 40, },
         { minlevel = 401, maxlevel = 600, multiplier = 30, },
-        { minlevel = 601, maxlevel = 800, multiplier = 20 },
-        { minlevel = 801, maxlevel = 1000, multiplier = 15 },
-        { minlevel = 1001, maxlevel = 1200, multiplier = 12 },
-        { minlevel = 1201, maxlevel = 1400, multiplier = 10 },
-        { minlevel = 1401, maxlevel = 1600, multiplier = 8 },
-        { minlevel = 1601, maxlevel = 1800, multiplier = 5 },
-        { minlevel = 1801, maxlevel = 2000, multiplier = 4 },
+        { minlevel = 601, maxlevel = 1000, multiplier = 20 },
+        { minlevel = 1001, maxlevel = 1400, multiplier = 18 },
+        { minlevel = 1401, maxlevel = 1600, multiplier = 15 },
+        { minlevel = 1601, maxlevel = 1800, multiplier = 12 },
+        { minlevel = 1801,  multiplier = 10 },
 }
 
 skillsStages = {
