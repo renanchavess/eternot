04ca56a752859de0c766e9fe178dd62fc7fdd872
Renan Chaves <renanchavess@gmail.com>
2025-10-24 00:31:05 -0300

    fix: estagios eventos

---
+++ data/stages_original.lua
@@ -0,0 +1,54 @@
+experienceStages = {
+        { minlevel = 1, maxlevel = 200, multiplier = 50, },
+        { minlevel = 201, maxlevel = 400, multiplier = 40, },
+        { minlevel = 401, maxlevel = 600, multiplier = 30, },
+        { minlevel = 601, maxlevel = 1000, multiplier = 20 },
+        { minlevel = 1001, maxlevel = 1400, multiplier = 18 },
+        { minlevel = 1401, maxlevel = 1600, multiplier = 15 },
+        { minlevel = 1601, maxlevel = 1800, multiplier = 12 },
+        { minlevel = 1801,  multiplier = 10 },
+}
+
+skillsStages = {
+        {
+                minlevel = 0,
+                maxlevel = 80,
+                multiplier = 10,
+        },
+        {
+                minlevel = 81,
+                maxlevel = 100,
+                multiplier = 7,
+        },
+        {
+                minlevel = 101,
+                maxlevel = 120,
+                multiplier = 6,
+        },
+        {
+                minlevel = 121,
+                multiplier = 5,
+        }
+}
+
+magicLevelStages = {
+        {
+                minlevel = 0,
+                maxlevel = 40,
+                multiplier = 10,
+        },
+        {
+                minlevel = 41,
+                maxlevel = 100,
+                multiplier = 7,
+        },
+        {
+                minlevel = 101,
+                maxlevel = 120,
+                multiplier = 6,
+        },
+        {
+                minlevel = 121,
+                multiplier = 5,
+        }
+}
